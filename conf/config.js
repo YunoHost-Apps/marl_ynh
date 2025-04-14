a6bf300016100c7fbd28032e44de3ee627d8fac7
oleole39 <59071673+oleole39@users.noreply.github.com>
2025-04-15 00:15:26 +0200

    change config strategy and use config template

---
+++ conf/config.js
@@ -0,0 +1,17 @@
+/* 
+ * MARL configuration 
+ */
+
+// Empty array to disable server mode 
+const servers = [__ARCHIVES_PATHS__];
+
+// Server mode's preferences
+const customPrefs = {
+  "lang": __LANG__,
+  "theme": __THEME__,
+  "sortAsc": __SORT_ASC__,
+  "pageSize": __PAGE_SIZE__,
+  "combinePanels": __COMBINE_PANELS__,
+  "defaultPanel": __DEFAULT_PANELS__,
+  "simplifyPostsDisplay": __SIMPLIFY_POST_DISPLAY__
+};
